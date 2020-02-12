0074e74674ccc4f867e958d78694b842bb5ed326
aurelien <ahennuyer@datainmotion.fr>
2020-02-12 14:41:58 +0100

    first commit for react ecommerce

---
+++ src/api/api.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id:1,
+        title: 'Shenmue',
+        price: 50,
+        category: 'Dreamcast'
+    },
+    {
+        id: 2,
+        title: 'Pokemon Epée',
+        price: 60,
+        category: 'Switch',
+    },
+    {
+        id: 3,
+        title: 'Pokemon Bouclier',
+        price: 60,
+        category: 'Switch',
+    },
+    {
+        id: 4,
+        title: 'Dragon ball Tome 1',
+        price: 10,
+        category: 'Manga',
+    },
+    {
+        id: 5,
+        title: 'Nano block n°001',
+        price: 14,
+        category: 'Jouets',
+    },
+    {
+        id: 6,
+        title: 'Mug Thermo reactif Dragonball Z',
+        price: 20,
+        category: 'Produits dérivés',
+    }
+]
